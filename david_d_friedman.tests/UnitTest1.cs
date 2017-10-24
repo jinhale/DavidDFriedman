e521c234af0578d99142705ac515fb6a4048fb3e
Justin Leon Hale <haleleonj@outlook.com>
2017-10-24 19:04:15 -0400

    Add a working unit test

--- david_d_friedman.tests/UnitTest1.cs
+++ david_d_friedman.tests/UnitTest1.cs
@@ -1,5 +1,11 @@
 using System;
 using Xunit;
+using david_d_friedman.Controllers;
+
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using david_d_friedman.Models;
 
 namespace david_d_friedman.tests
 {
@@ -8,7 +14,12 @@ namespace david_d_friedman.tests
         [Fact]
         public void Test1()
         {
+            var home = new HomeController();
+            var actionResult = home.Index();
 
+	    Type b = typeof(IActionResult);
+            
+            Assert.True(actionResult != null);
         }
     }
 }
