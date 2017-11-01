4f2f8edbaec5e20d4a355531033707664c51503e
Justin Leon Hale <haleleonj@outlook.com>
2017-11-01 07:07:28 -0400

    Remove redundant code

--- david_d_friedman/Controllers/HomeController.cs
+++ david_d_friedman/Controllers/HomeController.cs
@@ -17,15 +17,11 @@ namespace david_d_friedman.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
